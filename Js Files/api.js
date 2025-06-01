5dcc8c8f462fa26f79be2e603f9c527c8add0360
Satyajeet-23 <satyajeett023@outlook.com>
2025-06-01 22:11:04 +0530

    Added, Updated & Completed api.js

--- Js Files/api.js
+++ Js Files/api.js
@@ -1,17 +1,34 @@
 "use strict";
 
-const api_key = "6693bcacb4d42ae0daebd358f50eebcd";
-
-/**
- * Fetch Data From Server
- 
- * @param {string} URL API URL
- * @param {Function} callback callback
-
-*/
+const api_key = "966e4c79e9fc73aa466bc83495b7a0df";
 
 export const fetchData = function (URL, callback) {
   fetch(`${URL}&appid=${api_key}`)
-    .then(res => res.json())
-    .then(data => callback(data));
-}
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      callback(data);
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  geo(query) {
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
